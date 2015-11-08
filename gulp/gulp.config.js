b99a01393baf6339ce615a4029b7c2c4290cee78
blackendstudios <jeroflat@gmail.com>
2015-11-08 01:13:38 -0500

    using a gulp configuration file

---
+++ gulp/gulp.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  app_files: {
+      indexHtml : ['public/index.html'],
+      tplFiles: ['public/components/**/*js']
+  }
+
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     runSequence = require('run-sequence'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    gulpConfig = require('./gulp/gulp.config.js');
 
 gulp.task('default', function (callback) {
    runSequence('build', callback);
@@ -15,9 +16,11 @@ gulp.task('clean', function () {
 });
 
 gulp.task('index', function (callback) {
-   var target = gulp.src('public/index.html'),
-       tpl_src = gulp.src('public/components/**/*js');
-   return target.pipe(inject(tpl_src)).pipe(gulp.dest('public/dist'));
+
+   return gulp.src(gulpConfig.app_files.indexHtml)
+      .pipe(inject(gulp.src(gulpConfig.app_files.tplFiles)))
+      .pipe(gulp.dest('public/dist'));
+
 });
 
 gulp.task('mainTask', ['task1', 'task2', 'task3'], function () {
